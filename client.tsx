820df26a67ffdb06039dcd0ea7289d8105bc06e1
Yeonji-Noob <rhcp3885@naver.com>
2023-07-18 13:28:49 +0900

    파일 수정

--- client.tsx
+++ client.tsx
@@ -2,10 +2,10 @@
 // tsconfig에서 "esModuleInterop": true 를 켜도 되지만 권장하지 않는다고 한다.
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { hot } from 'react-hot-loader/root';
+// import { hot } from 'react-hot-loader/root';
 
 import MineSearch from './components/MineSearch';
 
-const Hot = hot(MineSearch); //HOC
+// const Hot = hot(MineSearch); //HOC
 
-ReactDOM.render(<Hot />, document.querySelector('#root'));
+ReactDOM.render(<MineSearch />, document.querySelector('#root'));
